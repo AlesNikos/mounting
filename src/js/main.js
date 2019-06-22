c39ad12b90927da388df2beea0f1ee7a8088addf
Alexey Kostyuchik <alesnikos@gmail.com>
2019-06-22 18:05:48 +0500

    Исправил некоторые элементы и начал оптимизацию

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,38 @@
 $(document).ready(function() {
   
+  const dict = [
+    {
+      cardName: "card-1",
+      defaultText: "Монтаж систем <br>отопления",
+      activeText: "Здесь будет короткое описание о данной услуге"
+    },
+    {
+      cardName: "card-2",
+      defaultText: "Монтаж сантехнического<br>оборудования",
+      activeText: "Здесь будет короткое описание о данной услуге"
+    },
+    {
+      cardName: "card-3",
+      defaultText: "Монтаж канализационного<br>оборудования",
+      activeText: "Здесь будет короткое описание о данной услуге"
+    },
+    {
+      cardName: "card-4",
+      defaultText: "Монтаж систем <br>отопления",
+      activeText: "Здесь будет короткое описание о данной услуге"
+    },
+    {
+      cardName: "card-5",
+      defaultText: "Монтаж сантехнического<br>оборудования",
+      activeText: "Здесь будет короткое описание о данной услуге"
+    },
+    {
+      cardName: "card-6",
+      defaultText: "Монтаж канализационного<br>оборудования",
+      activeText: "Здесь будет короткое описание о данной услуге"
+    }
+  ];
+
   $('.navbar__icon').on('click', function (event) {
     event.preventDefault();
     $(this).closest('.navbar__list').toggleClass('navbar__list_open');
@@ -26,14 +59,47 @@ $(document).ready(function() {
     $('.modal-thanks').fadeOut();
   });
   /* Секция Услуги */
-  // $('.service__button').on('click', function(event) {
-  //   event.preventDefault();
-  //   // $('.service__text_active').fadeIn();
-  //   // $('.service__text').fadeOut();
-  //   // $('.service__card-1 .service__text').replaceWith('<p class="service__text_active">Здесь будет короткое описание <br>о данной услуге</p>');
-  //   $('.service__card-1 .service__text').toggleClass('service__text_active');
-  //   $('.service__text_active').html('Здесь будет короткое описание <br>о данной услуге');
-  // });
+  $('.service__button').on('click', function(event) {
+    event.preventDefault();
+    const target = event.target;
+    const clickedCard = target.getAttribute("data-target");
+
+    const findVal =  function (arr, clickedCard) {
+      for (let index = 0; index < arr.length; index++) {
+        const element = arr[index];
+        if (element.cardName === clickedCard) return element;        
+      }
+    }
+
+    const createNode = function(para, text) {
+      const words = text.split('<br>');
+
+      para.innerText = '';
+      for (let index = 0; index < words.length; index++) {
+        const element = words[index];
+        para.appendChild(document.createTextNode(element));
+        para.appendChild(document.createElement('br'));
+      }
+    }
+
+    const dictValue = findVal(dict, clickedCard);
+
+    const textElement = target.parentElement.querySelector('.service__text');
+
+    if (target.classList.contains('service__button_active')) {
+      target.classList.remove('service__button_active');
+      target.classList.add('service__button');
+      target.innerText = 'Узнать цену';
+      createNode(textElement, dictValue.defaultText);
+      textElement.classList.remove('service__text_active');
+    } else {
+      target.classList.add('service__button_active');
+      target.classList.remove('service__button');
+      target.innerText = 'от 500 р.';
+      createNode(textElement, dictValue.activeText);
+      textElement.classList.add('service__text_active');
+    }
+  });
 
   /* Настройка Slick Slider */
   /* $('.hero-slider').slick({
@@ -68,8 +134,8 @@ $(document).ready(function() {
     }
   });
 
-  /* Валидация форм */
-  $('#request-form').validate({
+/* Иницализируем отправку формы */
+  const validationParams = {
     rules: {
       user_name: {
         required: true,
@@ -87,49 +153,29 @@ $(document).ready(function() {
       user_phone: "Заполните поле"
     },
     errorClass: "invalid",
-    errorElement: "div"  
-  });
+    errorElement: "div",
+    submitHandler: function (form) {
+      $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(form).serialize(),
+        success: function () {
+          $(form).find("input").val("");
+          $('.modal').removeClass('modal_active');
+          $('.modal-thanks').fadeIn();
+          $("form").trigger("reset");
+        }
+      });
+      return false;
+    }
+  };
 
-  $('#modal-form').validate({
-    rules: {
-      user_name: {
-        required: true,
-        minlength: 2,
-        maxlength: 15
-      },
-      user_phone: "required"
-    },
-    messages: {
-      user_name: {
-        required: "Заполните поле",
-        minlength: jQuery.validator.format("Не менее {0} символов!"),
-        maxlength: jQuery.validator.format("Не более {0} символов!")
-      },
-      user_phone: "Заполните поле"
-    },
-    errorClass: "invalid",
-    errorElement: "div"
-  });
+  /* Валидация форм */
+  $('#request-form').validate(validationParams);
+  $('#modal-form').validate(validationParams);
 
   /* Маска для телефона */
   $('.phone').mask('+7 (999) 999-99-99');
-
-  /* Иницализируем отправку формы */
-  $('form').submit(function (event) {
-    event.preventDefault();
-    $.ajax({
-      type: "POST",
-      url: "mailer/smart.php",
-      data: $(this).serialize()
-    }).done(function () {
-      $(this).find("input").val("");
-      // alert("Ваша заявка принята");
-      $('.modal').fadeOut();
-      $('.modal-thanks').fadeIn();
-      $("form").trigger("reset");
-    });
-    return false;
-  });
 
   /* Плавная прокрутка к сециям по якорным ссылкам */
   $("#menu").on("click", "a", function (event) {
@@ -155,6 +201,21 @@ $(document).ready(function() {
       scrollTop: 0
     }, 600);
     return false;
+  });
+
+  /* Скрипт для появления карты при проктутке до нее */
+  var target = $('.footer');
+  var targetPos = target.offset().top;
+  var winHeight = $(window).height();
+  var scrollToElem = targetPos - winHeight;
+
+  $(window).scroll(function () {
+    var winScrollTop = $(this).scrollTop();
+    if (winScrollTop > scrollToElem) {
+      $('#map').html('<script async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Ad920aa478fad8781a4be5da55c1c9cdc746907b68ba93b41a4a4c07659ede650&amp;width=100%25&amp;height=666&amp;lang=ru_RU&amp;scroll=false"></script >');
+      // console.log('докрутили');
+      $(window).unbind('scroll');
+    }
   });
 
   new WOW().init();
